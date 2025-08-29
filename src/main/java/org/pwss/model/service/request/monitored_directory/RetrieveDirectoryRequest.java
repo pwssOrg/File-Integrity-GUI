80e5bf3261a9d38709f2886381002d70aaa0f334
Stefan <stefan-201@hotmail.com>
2025-08-29 18:40:56 +0200

    Added docs to the request records

--- src/main/java/org/pwss/model/service/request/monitored_directory/RetrieveDirectoryRequest.java
+++ src/main/java/org/pwss/model/service/request/monitored_directory/RetrieveDirectoryRequest.java
@@ -1,4 +1,9 @@
 package org.pwss.model.service.request.monitored_directory;
 
+/**
+ * Request record for retrieving a monitored directory by its ID.
+ *
+ * @param directoryId the ID of the monitored directory to retrieve
+ */
 public record RetrieveDirectoryRequest(long directoryId) {
 }
