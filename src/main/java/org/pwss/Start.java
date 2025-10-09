e3b0c85c3584e632347e3b21f0d419663b85b253
pwgit-create <snow_900@outlook.com>
2025-10-09 03:55:28 +0200

    Configuration Joyride :)

--- src/main/java/org/pwss/Start.java
+++ src/main/java/org/pwss/Start.java
@@ -1,6 +1,11 @@
 package org.pwss;
 
 import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.themes.FlatMacDarkLaf;
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+
 import java.awt.Image;
 import java.io.IOException;
 import java.util.Objects;
@@ -9,6 +14,8 @@ import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
+
+import org.pwss.app_settings.AppConfig;
 import org.pwss.controller.factory.AppControllerFactory;
 import org.pwss.controller.factory.ControllerFactory;
 import org.pwss.navigation.NavigationEvents;
@@ -21,11 +28,19 @@ public class Start {
     public static void main(String[] args) {
         final org.slf4j.Logger log = LoggerFactory.getLogger(Start.class);
         try {
-            FileIntegrityScannerSplashScreen.showSplash();
-            Thread.sleep(4000);
+            if (AppConfig.USE_SPLASH_SCREEN) {
+                FileIntegrityScannerSplashScreen.showSplash();
+                Thread.sleep(4000);
+            }
             // Set FlatLaf Look and Feel
-            UIManager.setLookAndFeel(new FlatDarculaLaf());
-
+            if (AppConfig.APP_THEME == 1)
+                UIManager.setLookAndFeel(new FlatDarculaLaf());
+            else if (AppConfig.APP_THEME == 2)
+                UIManager.setLookAndFeel(new FlatLightLaf());
+            else if (AppConfig.APP_THEME == 3)
+                UIManager.setLookAndFeel(new FlatMacLightLaf());
+            else if (AppConfig.APP_THEME == 4)
+                UIManager.setLookAndFeel(new FlatMacDarkLaf());
             // Create Main UI of the application on the Event Dispatch Thread
             SwingUtilities.invokeLater(() -> {
                 // Create the main frame
@@ -39,7 +54,7 @@ public class Start {
                     Image icon = ImageIO.read(Objects.requireNonNull(Start.class.getResource("/app-icon.png")));
                     frame.setIconImage(icon);
                 } catch (IOException e) {
-                    log.debug("Failed to load application icon",e);
+                    log.debug("Failed to load application icon", e);
                     log.error("Failed to load application icon: {}", e.getMessage());
                 }
 
---
+++ src/main/java/org/pwss/app_settings/ConfigLoader.java
@@ -0,0 +1,281 @@
+package org.pwss.app_settings;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import org.slf4j.LoggerFactory;
+
+/**
+ * The ConfigLoader class is responsible for loading configuration settings from
+ * a properties file and providing access to these settings.
+ * 
+ * @author PWSS ORG
+ */
+final class ConfigLoader {
+
+    private final org.slf4j.Logger log = LoggerFactory.getLogger(ConfigLoader.class);
+
+    /**
+     * Key in the properties file for splash screen setting.
+     */
+    private final String SPLASHSCREEN_KEY = "frontend.splashscreen";
+    /**
+     * Key in the properties file for application theme setting.
+     */
+    private String APP_THEME_KEY = "frontend.theme";
+    /**
+     * Key in the properties file for license key setting.
+     */
+    private final String LICENSE_KEY = "frontend.licensekey";
+
+    /**
+     * Path to the configuration file.
+     */
+    private final String CONFIG_FILE_PATH = "options" + File.separator + "app.config";
+    /**
+     * Properties object that holds the configuration data loaded from the
+     * properties file.
+     */
+    private final Properties properties = new Properties();
+
+    /**
+     * Flag indicating whether to use splash screen or not.
+     */
+    private final boolean useSplashScreen;
+    /**
+     * Value representing the application theme.
+     */
+    private final int appTheme;
+    /**
+     * License key for the application.
+     */
+    private final String licenseKey;
+
+    /**
+     * Constructor that loads configuration settings from the properties file and
+     * initializes
+     * the instance variables based on the loaded values. If loading fails, default
+     * values are used.
+     */
+    ConfigLoader() {
+
+        boolean result = loadConfig();
+
+        if (!result) {
+            this.useSplashScreen = true;
+            this.appTheme = 1;
+            this.licenseKey = "none";
+        } else {
+            this.useSplashScreen = getSplashScreenFlagFromConfigString(getSplashScreenProperty());
+            this.appTheme = getAppThemeValueFromConfigString(getAppThemeProperty());
+            this.licenseKey = getLicenseKeyProperty();
+        }
+
+    }
+
+    /**
+     * Parses the splash screen flag from a configuration string.
+     *
+     * @param configFileString The configuration string to be parsed
+     * @return falsee if the string is "false" (case insensitive), otherwise true
+     */
+    private final boolean getSplashScreenFlagFromConfigString(String configFileString) {
+
+        try {
+            if (configFileString.equalsIgnoreCase("false"))
+                return false;
+            else
+                return true;
+        } catch (Exception exception) {
+
+            log.debug("Splash screen flag from app settings could not be parsed", exception);
+            log.error("Splash screen flag from app settings could not be parsed {}", exception.getMessage());
+            return true;
+        }
+
+    }
+
+    /**
+     * Parses the application theme value from a configuration string.
+     *
+     * @param configFileString The configuration string to be parsed
+     * @return The integer value of the theme, or 1 if parsing fails or the value is
+     *         invalid
+     */
+    private final int getAppThemeValueFromConfigString(String configFileString) {
+
+        try {
+
+            int appThemeValue = Integer.parseInt(configFileString);
+
+            if (appThemeValue < 5)
+                return appThemeValue;
+            else
+                return 1;
+        }
+
+        catch (Exception exception) {
+            log.debug("Could not parse theme value from app settings", exception);
+            log.error("Could not parse theme value from app settings {}", exception.getMessage());
+            return 1;
+        }
+
+    }
+
+    /**
+     * Loads the configuration properties from the file specified by
+     * CONFIG_FILE_PATH.
+     *
+     * @return true if loading was successful, otherwise false
+     */
+    private final boolean loadConfig() {
+        try (FileInputStream input = new FileInputStream(CONFIG_FILE_PATH)) {
+            properties.load(input);
+            return true;
+        }
+
+        catch (Exception exception) {
+            log.debug("Could not load app settings", exception);
+            log.error("Could not load app settings", exception.getMessage());
+            return false;
+        }
+    }
+
+    /**
+     * Retrieves the splash screen property value from the properties file.
+     *
+     * @return The splash screen property value, or "true" if an error occurs
+     */
+    private final String getSplashScreenProperty() {
+        try {
+
+            String result = properties.getProperty(SPLASHSCREEN_KEY);
+            return result;
+
+        } catch (Exception exception) {
+
+            log.debug("Splash screen flag property could not be fetched", exception);
+            log.error("Splash screen flag property could not be fetched {}", exception.getMessage());
+            return "true";
+        }
+
+    }
+
+    /**
+     * Retrieves the application theme property value from the properties file.
+     *
+     * @return The application theme property value, or "1" if an error occurs
+     */
+    private final String getAppThemeProperty() {
+        try {
+
+            String result = properties.getProperty(APP_THEME_KEY);
+            return result;
+        } catch (Exception exception) {
+
+            log.debug("App theme property could not be fetched", exception);
+            log.error("App theme property could not be fetched {}", exception.getMessage());
+            return "1";
+        }
+    }
+
+    /**
+     * Retrieves the license key property value from the properties file.
+     *
+     * @return The license key property value, or "none" if an error occurs
+     */
+    private final String getLicenseKeyProperty() {
+        try {
+
+            String result = properties.getProperty(LICENSE_KEY);
+            return result;
+        } catch (Exception exception) {
+
+            log.debug("License key property could not be fetched", exception);
+            log.error("License key property could not be fetched {}", exception.getMessage());
+            return "1";
+        }
+
+    }
+
+    /**
+     * Sets the splash screen flag in the properties file.
+     *
+     * @param splashScreenFlag The value to set for the splash screen flag
+     * @return true if setting was successful, otherwise false
+     */
+    final boolean setSplashScreenFlag(String splashScreenFlag) {
+
+        try {
+            properties.setProperty(SPLASHSCREEN_KEY, splashScreenFlag);
+            return true;
+        } catch (Exception exception) {
+            log.debug("Splash screen flag could not be set in app.config file", exception);
+            log.error("Splash screen flag could not be set in app.config file", exception.getMessage());
+            return false;
+        }
+    }
+
+    /**
+     * Sets the application theme value in the properties file.
+     *
+     * @param appTheme The value to set for the application theme
+     * @return true if setting was successful, otherwise false
+     */
+    final boolean setAppTheme(String appTheme) {
+
+        try {
+            properties.setProperty(APP_THEME_KEY, appTheme);
+            return true;
+        } catch (Exception exception) {
+            log.debug("App theme could not be set in app.config file", exception);
+            log.error("App theme could not be set in app.config file", exception.getMessage());
+            return false;
+        }
+    }
+
+    /**
+     * Sets the license key value in the properties file.
+     *
+     * @param licenseKey The value to set for the license key
+     * @return true if setting was successful, otherwise false
+     */
+    final boolean setLicenseKey(String licenseKey) {
+        properties.setProperty(LICENSE_KEY, licenseKey);
+        try {
+            return true;
+        } catch (Exception exception) {
+            log.debug("License key could not be set in app.config file", exception);
+            log.error("License key could not be set in app.config file", exception.getMessage());
+            return false;
+        }
+    }
+
+    /**
+     * Gets the value indicating whether to use splash screen or not.
+     *
+     * @return true if splash screen is enabled, otherwise false
+     */
+    final boolean isUseSplashScreen() {
+        return useSplashScreen;
+    }
+
+    /**
+     * Gets the application theme value.
+     *
+     * @return The current application theme value
+     */
+    final int getAppTheme() {
+        return appTheme;
+    }
+
+    /**
+     * Gets the license key value.
+     *
+     * @return The current license key value
+     */
+    final String getLicenseKey() {
+        return licenseKey;
+    }
+}
