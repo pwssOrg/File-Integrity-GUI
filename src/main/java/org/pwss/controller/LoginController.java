fc19f69e20d8bb903660bfadd591827dd9139827
Stefan <stefan-201@hotmail.com>
2025-11-16 16:28:25 +0100

    Update user creation text

--- src/main/java/org/pwss/controller/LoginController.java
+++ src/main/java/org/pwss/controller/LoginController.java
@@ -152,7 +152,7 @@ public class LoginController extends BaseController<LoginScreen> {
                     """
                             Welcome to Integrity Hash!
                             
-                            You are about to create the first user for this application.
+                            You are about to create a user for this application.
                             Please make sure to remember your credentials as they will be required for future logins.
                             
                             Do you want to proceed?""",
