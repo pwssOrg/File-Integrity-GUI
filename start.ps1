ef8af9b038588bdcb472685ce044f6e385791c0c
Stefan <stefan-201@hotmail.com>
2025-11-21 16:48:47 +0100

    Update start script

--- start.ps1
+++ start.ps1
@@ -8,14 +8,14 @@ if (-not (Test-Path -Path "..\artifacts")) {
 
 if ($null -eq $portInUse) {
     Write-Host "Nothing is running on port 15400. Starting the process..."
-    java -jar "..\File-Integrity-Scanner\File-Integrity-Scanner\target\File-Integrity-Scanner-1.7.jar" &
+    java -jar "..\File-Integrity-Scanner\File-Integrity-Scanner\target\File-Integrity-Scanner-1.8.1.jar" &
     Write-Host "File-Integrity-Scanner started."
-    cp .\target\integrity_hash-1.1-jar-with-dependencies.jar ..\artifacts
-    java -jar ..\artifacts\integrity_hash-1.1-jar-with-dependencies.jar
+    cp .\target\integrity_hash-1.2-jar-with-dependencies.jar ..\artifacts
+    java -jar ..\artifacts\integrity_hash-1.2-jar-with-dependencies.jar
     exit
 } else {
     Write-Host "File-Integrity-Scanner is already running on port 15400."
-    cp .\target\integrity_hash-1.1-jar-with-dependencies.jar ..\artifacts
-    java -jar ..\artifacts\integrity_hash-1.1-jar-with-dependencies.jar
+    cp .\target\integrity_hash-1.2-jar-with-dependencies.jar ..\artifacts
+    java -jar ..\artifacts\integrity_hash-1.2-jar-with-dependencies.jar
     exit
 }
